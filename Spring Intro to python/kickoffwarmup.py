5f4a8da7ead98527e09a53fab23a9a7a0cf08c2b
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-01-31 16:25:30 +0000

    kickoff assignment

--- Spring Intro to python/kickoffwarmup.py
+++ Spring Intro to python/kickoffwarmup.py
@@ -1,4 +1,4 @@
 print("I believe a computer program Is like a program that helps people really get the feeling of what they can do like how they actually work
     ")
- "My favorite food Is breakfast food specifically eggs because they taste good and  they're easy to make . I make my eggs 1. by cracking the mand putting them Into a bowl , 2. I whisk them , 3. I put them Into a a pan then let them cook for like 5 min , 4. I put Black pepper In them ,5. I put string cheese in the or you a can put American cheese In them your choice 
+ "My   favorite food Is breakfast food specifically eggs because they taste good and  they're easy to make . I make my eggs 1. by cracking the mand putting them Into a bowl , 2. I whisk them , 3. I put them Into a a pan then let them cook for like 5 min , 4. I put Black pepper In them ,5. I put string cheese in the or you a can put American cheese In them your choice 
  ,6. I flip them and let it cook on each side , 7. lastly I chop them up put them n my plate with syrup and Im done . "
