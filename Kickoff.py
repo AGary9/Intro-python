cfb41b81557cb27805b166baaaa93d57ee5d3521
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-02-01 15:35:08 +0000

    daily assignment

--- Kickoff.py
+++ Kickoff.py
@@ -1,1 +1,26 @@
 "Ahmaj Gary"
+In your own words describe what a variable is?
+
+What is the difference between a float and an integer?
+
+In your own words, describe what the boolean data type is?
+
+Create four (4) variables and use the appropriate data type for each.
+
+1 variable for you full name.
+1 variable for your age.
+1 variable for your GPA.
+1 variable for if you are a student of Boys Latin.
+Describe a time where a piece of technology helped you or someoene you know in an unexpected way.
+
+Describe what it would be like for you to lose access to a technology that you use on daily basis. Describe what you would need to do differently to replace that missing technology.
+    a   variable is a substitution of something.
+The difference between a float and a interger is that a floating number has a decimal place and  a interger is really called a int.
+a boolean data type  can only have one value .
+w = x = y = z = 1 2 3 4
+(a - h - m - a j )
+a= 1  h = 5 (15)
+a =1 w= . r=3 v=3 f=0
+q= y u= e h=s
+My phone helped me in a unexpected way because i was lost and i hd remebered i had a gps on it and i used it to get where i needed to go .
+I would probably be bored if i lost technology , I would need entertain myself different like watching tv because itll be gone .
