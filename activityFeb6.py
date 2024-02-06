289119cff074c1b95a49bb8b2fb2b67828ec7020
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-02-06 16:20:42 +0000

    2/6/24

---
+++ activityFeb6.py
@@ -0,0 +1,32 @@
+# Q ; What Is complexity ?
+A : Measure of how reasource Intensive program Is .
+# Q ; What Is abstraction ?
+Answer = A : Abstraction Is the process of simplyifying or hiding complexity .
+(Notes) Print function outputs Data.
+(Notes) Input function bascically puts In data .
+Print ('Hello')
+Datafromuser= Input (' What is your name') # Puts In Data 
+Print ( DataFromUser)
+Print ( 200 )
+name= Input ( ' what is your name ')
+teeshirtsize = Imput ('What Is your tee shirt size')
+TeeshirtsizeSm= False
+TeeshirtsizeMd= False
+TeeshirtsizeLg= True 
+# camel Case= every new word after the first word Is capitalized.
+Characterobject = [ 3903 ,  'Sot''Jump']
+Characterobject movement=''
+Characterobject attack= ''
+Characterobject Interaction=''
+CharaacterobjectJump =''
+# SnakeCase = every new word after the first word gets an Underscore . Underscore Is the unofficial space bar .
+#PascalCase= evry word Is capitalized Including the first word .
+GuestinMath=
+GuestInLit=
+GuestInEnglish=
+GuestInPhysics=
+#1 names should be short and descriptive
+#2 names can start with a  underscore but not numbers non - alphabetical .
+# arimatic actions 
+Var0ne= Int(Input( ( ' please enter  number )')
+varTwo = Int (Input ( ))                  
