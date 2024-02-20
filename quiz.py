15b120f75557e58ab263ed21e2f23b533a015d31
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-02-20 15:41:07 +0000

    Quiz.py

---
+++ quiz.py
@@ -0,0 +1,76 @@
+
+#1. What built-in function would you use if you wanted
+# to print out data to a user? 
+
+you would use the input function
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+
+you would use parameters.
+
+#3. Explain what complexity and abstraction are, then
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+complexity abstractions are like taking away the important feautures of something that needs it , for example ps5 , the ps5 has a harddrvie that have a specific amount of storation that it can go up to .
+#4. What does it mean when you encounter a syntax error?
+If you encounter a syntax error you will have to put the qoutation marks on each end of your sentece and the begining of it .
+#5. What is a variable? Provide an example of a variable in code.
+
+A variable is like a substitution to get your answer , for example 2+x=5 in this case x is 3 .
+#6. Write a function in Python that when called will print out 
+# your name.
+
+Print('Ahmaj Gary')
+
+#7. What is a function definition and a function call?
+
+the definition of a function is something that works perfectly , a function call is bascically printing something seperatly and then at the end the computer puts it all together .
+#8. Describe what each symbol means?
+
+# = // single equal
+1 =
+# == // double equal
+2=
+# != // exclaimation equal
+!=
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+the output for the following codes are 20 .
+
+print(listOne == listTwo)
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag
+             you would find books , paper , pencils , loose paper , binder ,and laptop
+
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+
+bokks mayber 5 dollars , a stack of paper mayber 2 dollars or a dollar , a binder might be 10 dollars , and a laptop is 100 dollars 
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+booleans , sequence ,and text .
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+less than or greater than , you could use that scenario .
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+and or and not , you can use that scenario, 
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data: 2 is less than 3
+currentScore= 100
+highScore=2300
+
+# Bonus : What does the type() function do?
+its tells the computer what you want it to print . 
