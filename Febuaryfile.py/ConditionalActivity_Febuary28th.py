50e5354e16aebe0092fb07601b9792eb338cad4b
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-02-28 16:22:24 +0000

    ConditionalActivityClassworkFeb,28th.

---
+++ Febuaryfile.py/ConditionalActivity_Febuary28th.py
@@ -0,0 +1,11 @@
+# Engineer 
+
+Passcode:(Get_Money)
+
+CorrectPassocode:(get_this_Money)
+
+IncorrectPasscodeImportedbyuser:(Get_this_bread)
+
+X=(Get_this_money)=CorrectPassocode
+
+'Get_this_money'
---
+++ Febuaryfile.py/Warmup_Febuary,28th.py
@@ -0,0 +1,5 @@
+Print goodmorning(name) = ' example of a bad function.'
+
+' The error In this function as far as I can see Is Is that after print it Doesnt have these Signs : after Print , or anything qoutation marks so that would be the type error , and lastly It doesnt have a name to print where there should be a name so it wouldnt be able to print anything.'
+
+'The difference between a function arguement and a function paremeter Is that a funtion Paremeter is the names of list functions , Where as though the function arguenments are real vaues passed to the function. '
