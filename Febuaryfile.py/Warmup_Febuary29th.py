275d19086f9afc9b407a161e664f3412fd20d166
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-02-29 16:17:10 +0000

    ConditionalWarmupWork.

---
+++ Febuaryfile.py/Warmup_Febuary29th.py
@@ -0,0 +1,23 @@
+'The condtional Statements If / Else they worlk by communication with the Computor and telling it how the program should go through/Work'
+
+# condtional functions 
+
+# If X=45
+
+# X>45
+
+Print:('X Is Greater than 45')
+
+# Else X=45
+
+# x < 45
+
+Print:('45 Is greater than X')
+
+' Conditional Statement = Is a construct that evaluates , different conde conditons and outputs specific outcomes.'
+
+'evauluate Specific conditions'
+'and will output Specific outcome'
+
+# If/Else are functions that have been abstracted.
+# to make it easier for engineers to use logic.
