8e3fe7926e6380eceebb574c3d5a9f711c8b583f
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-03-01 16:05:49 +0000

    MarchWarmup.py

---
+++ Febuaryfile.py/MarchMonth.py/Warmup_March,1st.2024.py
@@ -0,0 +1,10 @@
+# Clues ank Keyboard
+# Conditional Statements IF/ ELSE/ ELIF
+# Input () a number for the grade - Interger.
+# Print () a letter for the grade - String.
+# Operators you need to use comparison ( Less than , Greater than )
+# need to use a arguement to pas a numerical Grade - Interger.
+# We need to make a function 
+# def Gradebook ( StudentGrade)
+
+# If StudentGrade > = 60:
