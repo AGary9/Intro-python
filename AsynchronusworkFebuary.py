fc0fdec81a138a65e3b948514c6bd1661b1483c5
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-03-12 15:24:35 +0000

    Classwork

--- AsynchronusworkFebuary.py
+++ AsynchronusworkFebuary.py
@@ -2,9 +2,9 @@
 
 ## Create a new file called feb21async.py, copy the questions from this document into your new document,  then watch the following videos and respond to the following questions.
 
-- You may work with your classmates to discuss and answer the questions. 
-- Your responses must be written in complete sentences. 
-- Your responses must be written by YOU- do not copy and paste answers from the web. 
+# You may work with your classmates to discuss and answer the questions. 
+# Your responses must be written in complete sentences. 
+# Your responses must be written by YOU- do not copy and paste answers from the web. 
 
 Once completed, commit and sync your work to github. This will count toward your grade. 
 
---
+++ ExtraCreditAsynchclasswork.py
@@ -0,0 +1,7 @@
+# I believe technolodgy has Influenced and shaped how you learn In school by it helping others how to problem Solve by themselves and to  understand thing more clearly and efficiant.
+#I believe this has positive Influence. I say this because like I stated above it will help you Understnad things more clearly than rather than ask multiple people for help , then from now on you can Just do it on your own.
+
+# Some ways technolodgy can help people learn by the internet it helps people explore there options and easily find the answers to the questions mostly by using google , this really helps a lot. I also say you can messgage people that knows the work you doing and that has experience of what your doing like back then people didnt have technolodgy like this to do the stuff we can today.
+# Lastly I syay you can reasarch people like to find out more about them or what they do.
+
+# For example for the first reason I provided you may not know a answer and then you can Just find your answer. For the second reason I say if google doesnt answer your question you can Just message them and ask them to help you. For the Last reason say for a project you  can reasearch the person and get facts on them to do your assignment.
