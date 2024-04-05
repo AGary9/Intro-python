236bc55045415c66e5eb47f12e2a5b3c38cf3ff5
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-04-05 15:24:15 +0000

    work

---
+++ Warmup.py
@@ -0,0 +1,18 @@
+# A python loop Is something that continues over and over Until what your trying to conditioned Is Satisfied.
+
+# In a python loop a Iterator is the thing that helps It repeat.
+
+# I would fix this block of code by using a return statement.
+
+# While a control construct that will run a block of code repeatedly , so long as the condition is true.
+
+# Short definiton: A while loop Is a function that will run a block of code repeadetly as the condition is true.
+
+msg = 'hello'
+
+While msg == 'hello' # (So long as this is true)
+#msg Is equal too hello
+      print(msg) # repeat this 
+# msg == ' goodbye '
+ return msg
+Print(' Goodbye')
