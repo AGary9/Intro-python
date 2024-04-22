4d8d3ce6128e829727a9f06868dc6a6a7bd228fc
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-04-22 14:35:02 +0000

    Asynchwork.py

---
+++ April , 22 , 24 AsynchWork.py
@@ -0,0 +1,33 @@
+Read the following prompt and answer the 3 questions
+
+# Prompt
+
+# You have been tasked as an engineer to develop a program that controls the lights in a small home.
+# When the users uses the lighting control app that you are developing, they will have the ability to 
+# turn lights on and off in the entire house. They can turn lights on and off based on the room in the home 
+# individually or all the lights in the home at the same time. 
+# there are 3 bedrooms, 1 kitchen, 2 bathrooms, a living room, a dining room, and a basement that all
+# have lights and can all be manipulated with by using this app. 
+
+# Questions
+
+# 1. Based on the prompt you just read, what is the main goal(s) of the app that you are creating?
+# What is the primary objective of your app? Please write your response using complete a complete sentence.
+
+# Based off what I have Just read I believe the primary goal of the app Is to be able to manipulate lights in each room that was said in the prompt , the main goals Is to have lights Inside the whole house to be able to see.
+
+
+# 2. What type of research would you need to do to help you develop this app? And are there 
+# any questions that you can think of that could give you more context/ information
+# that can help you develop this program that wasn't included ? 
+# Please write your response in at least 4 complete sentences. 
+
+# The type of reasearch that I would try to find Is maybe go on youtube and search how to work an app like this. One question that I would ask Is that how would a professional work an app like this. I also would use the reasearch and take notes. Also constantly study the notes so I can be somewhat good at in manufacturing this app.
+
+
+# 3. Breakdown step- by- step how you would create this program in Python? Your steps do not have
+# to be written in code and it does not have to use python keywords- rather your steps 
+# should be explained in a way where an engineer can take the information and understand
+# what they need to do.  Please be sure to write your steps in complete sentences. 
+
+# Well In Python I may Just start off In Writting a functon. For Example I would also Collect Input of a user , Based off of googel reasearch I would also use a binding function and then run the app or take more steps.
